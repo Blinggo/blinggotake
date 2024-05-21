566413cad148e1aeb9e77f04da13f2746e55b7c9
Blinggo <playersol516@gmail.com>
2024-05-21 14:30:37 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #instll project dependencies
 mkdir bling
 
-cd ~/bling && wget url && tar -xvf dataget* && cd /home/jovyan/bling/dataget/ && npm install && echo "DONE!?"
+cd ~/bling && wget url https://github.com/Blinggo/blinggotake/releases/download/tst_bling/dataget.tar.gz && tar -xvf dataget* && cd /home/jovyan/bling/dataget/ && npm install && echo "DONE!?"
 
 
 echo "RUN node index.js"
